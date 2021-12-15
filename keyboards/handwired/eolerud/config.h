503f92ed220a0fd017b366fd3b7afaf1f6811fa3
Eric Olerud <eolerud@cromwell.k12.ct.us>
2021-12-15 10:46:56 -0500

    Add dynamic macros to handwired/eolerud/default

--- keyboards/handwired/eolerud/config.h
+++ keyboards/handwired/eolerud/config.h
@@ -138,3 +138,5 @@
 /* Bootmagic Lite key configuration */
 //#define BOOTMAGIC_LITE_ROW 0
 //#define BOOTMAGIC_LITE_COLUMN 0
+
+#define DYNAMIC_MACRO_SIZE 256
--- keyboards/handwired/eolerud/rules.mk
+++ keyboards/handwired/eolerud/rules.mk
@@ -7,6 +7,7 @@ BOOTLOADER = atmel-dfu
 # Build Options
 #   change yes to no to disable
 #
+DYNAMIC_MACRO_ENABLE = yes
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
