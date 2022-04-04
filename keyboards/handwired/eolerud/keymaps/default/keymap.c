24caa96d0302f662be93d7042a224c3b9362c833
Eric Olerud <ericolerud@protonmail.com>
2022-04-04 00:19:42 -0400

    add insert key

--- keyboards/handwired/eolerud/keymaps/default/keymap.c
+++ keyboards/handwired/eolerud/keymaps/default/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Raise */
     [_RAISE] = LAYOUT(
       _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11, KC_F12,               KC_DEL,
-	       _______,  _______, SELWORD, _______,   RESET, _______, _______,  TICKET, _______, _______, KC_PSCR, _______, _______,   _______,
+	       _______,  _______, SELWORD, _______,   RESET, _______, _______,  TICKET,  KC_INS, _______, KC_PSCR, _______, _______,   _______,
                _______,  _______, _______, _______, _______, _______, KC_HOME, _______, _______, _______,    _______,  _______,        KC_LEAD,
 	       _______,   DM_PLY1, DM_PLY2, DM_RSTP, DM_REC1, DM_REC2,  KC_END, _______,    _______,_______,  _______,                 _______,            KC_MS_U,
 	       _______, _______, _______,                          DASH,                         _______,    _______, _______,         _______,   KC_MS_L, KC_MS_D, KC_MS_R
